1372d7ab8b14bc5a320d7e565f9e2e5e3086f289
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-10 11:25:24 +0900

    function

---
+++ UnityBasic1/Assets/FunctionExample.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class FunctionExample : MonoBehaviour
+{
+    // Function
+    void SayHello()
+    {
+        Debug.Log("Hello, Unity");
+    }
+
+    int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    void Start()
+    {
+        SayHello();
+        int total = AddNumbers(3, 5);
+        Debug.Log($"Total: {total}");
+    }
+
+    void Update()
+    {
+             
+    }
+}
