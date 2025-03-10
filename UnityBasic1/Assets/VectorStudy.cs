932ce37137b32230b08dbba6dd135e38004a6fd4
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-10 13:31:47 +0900

    [day12-vector] normalize

--- UnityBasic1/Assets/VectorStudy.cs
+++ UnityBasic1/Assets/VectorStudy.cs
@@ -7,14 +7,25 @@ public class VectorStudy : MonoBehaviour
 
     void Start()
     {
-        Vector3 a = new Vector3(1, 0, 0);
-        Vector3 b = new Vector3(2, 0, 0);
+        //{
+        //    Vector3 a = new Vector3(1, 0, 0);
+        //    Vector3 b = new Vector3(2, 0, 0);
 
-        Vector3 c = a + b;
+        //    Vector3 c = a + b;
 
-        Debug.Log("Vector" + c);
+        //    Debug.Log("Vector" + c);
+
+        //    Debug.Log("Length: " c.magnitude);
+        //}
+
+        Vector3 a = new Vector3(3, 0, 0);
+
+        // 1. 정규화 normalize
+        // 벡터의 크기를 1로 만들고 방향만 유지
+        Vector3 normalizedVector = a.normalized;
+
+        Debug.Log("1의 길이 방향만 설정하는 정규화: " + normalizedVector);
     }
-
     void Update()
     {
         
