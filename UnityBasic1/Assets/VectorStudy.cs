a85df515aa7d1bf1cb7aa2e745951cffcaaa0bf6
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-10 13:23:34 +0900

    [day12-vector] adding vectors

--- UnityBasic1/Assets/VectorStudy.cs
+++ UnityBasic1/Assets/VectorStudy.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class VectorStudy : MonoBehaviour
 {
-    public Vector2 v2 = new Vector2(10, 10);
-    public Vector3 v3 = new Vector3(1, 1, 1);
+    // public Vector2 v2 = new Vector2(10, 10);
+    // public Vector3 v3 = new Vector3(1, 1, 1);
 
     void Start()
     {
-        
+        Vector3 a = new Vector3(1, 0, 0);
+        Vector3 b = new Vector3(2, 0, 0);
+
+        Vector3 c = a + b;
+
+        Debug.Log("Vector" + c);
     }
 
     void Update()
