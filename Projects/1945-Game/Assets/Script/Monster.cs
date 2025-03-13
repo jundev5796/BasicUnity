2b91b0480e0d5cf6d0f50f240fa183c2bfcb2442
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-13 11:49:15 +0900

    [1945-Game] control enemy bullet speed and delay

--- Projects/1945-Game/Assets/Script/Monster.cs
+++ Projects/1945-Game/Assets/Script/Monster.cs
@@ -4,13 +4,23 @@ public class Monster : MonoBehaviour
 {
     public float Speed = 3.0f;
     public float Delay = 1f;
-    // public Transform ms1;
-    // public Transform ms2;
-    // public GameObject bullet;
-    
+    public Transform ms1;
+    public Transform ms2;
+    public GameObject bullet;
+
     void Start()
     {
-        
+        // 한번함수호출
+        Invoke("CreateBullet", Delay);
+    }
+
+    void CreateBullet()
+    {
+        Instantiate(bullet, ms1.position, Quaternion.identity);
+        Instantiate(bullet, ms2.position, Quaternion.identity);
+
+        // 재귀호출
+        Invoke("CreateBullet", Delay);
     }
     
     void Update()
