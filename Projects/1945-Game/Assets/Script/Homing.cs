5fefaa787ccdc5b787cbf6c555af70561309f6a5
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-13 14:59:27 +0900

    [1945-Game] implement homing bullet on mid-size enemy

--- Projects/1945-Game/Assets/Script/Homing.cs
+++ Projects/1945-Game/Assets/Script/Homing.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Homing : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject target;  //플레이어
+    public float Speed = 3f;
+    Vector2 dir;
+    Vector2 dirNo;
+
+
     void Start()
     {
-        
+        //플레이어 태그로 찾기
+        target = GameObject.FindGameObjectWithTag("Player");
+        //A - B  A바라보는 벡터     플레이어 - 미사일 
+        dir = target.transform.position - transform.position;
+        //방향벡터만 구하기 단위벡터 정규화 노말 1의 크기로 만든다.
+        dirNo = dir.normalized;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        transform.Translate(dirNo * Speed * Time.deltaTime);
+
+        //transform.position = Vector3.MoveTowards(transform.position
+        //    , target.transform.position, Speed * Time.deltaTime);
     }
 }
