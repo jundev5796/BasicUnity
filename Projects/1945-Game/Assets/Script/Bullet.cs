ad99cc3a5d793705170eb82aa4f04c8bda307d50
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-13 10:41:53 +0900

    [1945-Game] create and shoot bullet

---
+++ Projects/1945-Game/Assets/Script/Bullet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed = 4.0f;
+    // 공격력
+    // 이텍트
+
+    void Update()
+    {
+        // 미사일 위쪽방향으로 움직이기
+        // 위의 방향 * 스피드 * 타임
+        transform.Translate(Vector2.up * Speed * Time.deltaTime);
+    }
+
+    // 화면밖으로 나갈경우
+    private void OnBecameInvisible()
+    {
+        // 자기 자신 지우기
+        Destroy(gameObject);
+    }
+}
