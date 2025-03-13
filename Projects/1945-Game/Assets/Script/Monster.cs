54a9b9e1257463eb732857eac0de4a24b3b5afc3
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-13 11:10:19 +0900

    [1945-Game] enemy move

--- Projects/1945-Game/Assets/Script/Monster.cs
+++ Projects/1945-Game/Assets/Script/Monster.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    public float Speed = 3.0f;
+    public float Delay = 1f;
+    // public Transform ms1;
+    // public Transform ms2;
+    // public GameObject bullet;
     
     void Start()
     {
         
     }
-
     
     void Update()
     {
-        
+        // 아래 방향으로 움직여라
+        transform.Translate(Vector2.down * Speed * Time.deltaTime);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
