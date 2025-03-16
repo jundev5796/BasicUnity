e71cfa20dfc7d32fddba7bed5fb80f983f041d72
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-16 12:46:59 +0900

    [1945-Game(Practice3)] create background and player

---
+++ Projects/Practice/1945-Game(Practice3)/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetAxis("Horizontal") <= -0.5f)
+            anim.SetBool("Left", true);
+        else
+            anim.SetBool("Left", false);
+
+        if (Input.GetAxis("Horizontal") >= 0.5f)
+            anim.SetBool("Right", true);
+        else
+            anim.SetBool("Right", false);
+
+        if (Input.GetAxis("Vertical") >= 0.5f)
+            anim.SetBool("Up", true);
+        else
+            anim.SetBool("Up", false);
+
+        transform.Translate(moveX, moveY, 0);
+
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        viewPos.x = Mathf.Clamp01(viewPos.x); //x값을 0이상, 1이하로 제한한다.
+        viewPos.y = Mathf.Clamp01(viewPos.y); //y값을 0이상, 1이하로 제한한다.
+        Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewPos);//다시월드좌표로 변환
+        transform.position = worldPos; //좌표를 적용한다.
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
